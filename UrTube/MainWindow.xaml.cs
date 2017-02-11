385d02a56cf2ff3807f5c30f91385c2bfd96e708
Makakura <Makakura@DESKTOP-PKJH420>
2017-02-11 20:22:24 +0700

    a

--- UrTube/MainWindow.xaml.cs
+++ UrTube/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
 
 namespace UrTube
 {
@@ -20,14 +22,128 @@ namespace UrTube
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Page 2 var
+        List<Google.Apis.YouTube.v3.Data.SearchResult> lstResult;
+        //End Page 2 var
         public MainWindow()
         {
             InitializeComponent();
-            mainFrame.Navigate(new Uri("Page2.xaml", UriKind.Relative));
+            ChangeState(1);
+            lstResult = new List<Google.Apis.YouTube.v3.Data.SearchResult>();
         }
+        //Page 2 Method
+        private async void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            await Run();
+        }
+        private async Task Run()
+        {
+            try
+            {
+                var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+                {
+                    ApiKey = "AIzaSyC5IkscZNJZCEQ_Z8shR80_vi0hFkYS07o",
+                    ApplicationName = this.GetType().ToString()
+                });
+
+                var searchListRequest = youtubeService.Search.List("snippet");
+                searchListRequest.Q = "Makakura"; // Replace with your search term.
+                searchListRequest.MaxResults = 50;
+
+                // Call the search.list method to retrieve results matching the specified query term.
+                var searchListResponse = await searchListRequest.ExecuteAsync();
+
+                // Add each result to the appropriate list, and then display the lists of
+                // matching videos, channels, and playlists.
+                foreach (var searchResult in searchListResponse.Items)
+                {
+                    if (searchResult.Id.Kind == "youtube#video")
+                        lstResult.Add(searchResult);
+                }
+                lvVideoResult.ItemsSource = lstResult;
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+        //End Page 2 method
         private void TextBlock_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+        private void ChangeState(int state)
+        {
+            switch (state)
+            {
+                case 1:
+                    grLogin.Visibility = Visibility.Visible;
+                    grPickResource.Visibility = Visibility.Collapsed;
+                    tblState1.SetValue(TextBlock.FontWeightProperty, FontWeights.Bold);
+                    tblState2.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState3.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState4.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState1.SetValue(TextBlock.FontSizeProperty, 20);
+                    tblState2.SetValue(TextBlock.FontSizeProperty, 16);
+                    tblState3.SetValue(TextBlock.FontSizeProperty, 16);
+                    tblState4.SetValue(TextBlock.FontSizeProperty, 16);
+                    grStateLine1.Visibility = Visibility.Visible;
+                    grStateLine2.Visibility = Visibility.Collapsed;
+                    grStateLine3.Visibility = Visibility.Collapsed;
+                    grStateLine4.Visibility = Visibility.Collapsed;
+                    break;
+                case 2:
+                    grLogin.Visibility = Visibility.Collapsed;
+                    grPickResource.Visibility = Visibility.Visible;
+                    tblState2.SetValue(TextBlock.FontWeightProperty, FontWeights.Bold);
+                    tblState1.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState3.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState4.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState2.SetValue(TextBlock.FontSizeProperty, 20);
+                    tblState1.SetValue(TextBlock.FontSizeProperty, 16);
+                    tblState3.SetValue(TextBlock.FontSizeProperty, 16);
+                    tblState4.SetValue(TextBlock.FontSizeProperty, 16);
+                    grStateLine2.Visibility = Visibility.Visible;
+                    grStateLine1.Visibility = Visibility.Collapsed;
+                    grStateLine3.Visibility = Visibility.Collapsed;
+                    grStateLine4.Visibility = Visibility.Collapsed;
+                    break;
+                case 3:
+                    tblState3.SetValue(TextBlock.FontWeightProperty, FontWeights.Bold);
+                    tblState2.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState1.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState4.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState3.SetValue(TextBlock.FontSizeProperty, 20);
+                    tblState2.SetValue(TextBlock.FontSizeProperty, 16);
+                    tblState1.SetValue(TextBlock.FontSizeProperty, 16);
+                    tblState4.SetValue(TextBlock.FontSizeProperty, 16);
+                    grStateLine3.Visibility = Visibility.Visible;
+                    grStateLine1.Visibility = Visibility.Collapsed;
+                    grStateLine2.Visibility = Visibility.Collapsed;
+                    grStateLine4.Visibility = Visibility.Collapsed;
+                    break;
+                case 4:
+                    tblState4.SetValue(TextBlock.FontWeightProperty, FontWeights.Bold);
+                    tblState1.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState2.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState3.SetValue(TextBlock.FontWeightProperty, FontWeights.Regular);
+                    tblState4.SetValue(TextBlock.FontSizeProperty, 20);
+                    tblState1.SetValue(TextBlock.FontSizeProperty, 16);
+                    tblState2.SetValue(TextBlock.FontSizeProperty, 16);
+                    tblState3.SetValue(TextBlock.FontSizeProperty, 16);
+                    grStateLine4.Visibility = Visibility.Visible;
+                    grStateLine1.Visibility = Visibility.Collapsed;
+                    grStateLine2.Visibility = Visibility.Collapsed;
+                    grStateLine3.Visibility = Visibility.Collapsed;
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeState(2);
         }
     }
 }
--- UrTube/Page2.xaml.cs
+++ UrTube/Page2.xaml.cs
@@ -13,38 +13,47 @@ namespace UrTube
     public partial class Page2 : Page
     {
         List<Google.Apis.YouTube.v3.Data.SearchResult> lstResult;
+
         public Page2()
         {
             InitializeComponent();
+            lstResult = new List<Google.Apis.YouTube.v3.Data.SearchResult>();
         }
 
-        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        private async void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            Run().Wait();
+            await Run();
         }
         private async Task Run()
         {
-            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            try
             {
-                ApiKey = "AIzaSyC5IkscZNJZCEQ_Z8shR80_vi0hFkYS07o",
-                ApplicationName = this.GetType().ToString()
-            });
+                var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+                {
+                    ApiKey = "AIzaSyC5IkscZNJZCEQ_Z8shR80_vi0hFkYS07o",
+                    ApplicationName = this.GetType().ToString()
+                });
 
-            var searchListRequest = youtubeService.Search.List("snippet");
-            searchListRequest.Q = "Makakura"; // Replace with your search term.
-            searchListRequest.MaxResults = 50;
+                var searchListRequest = youtubeService.Search.List("snippet");
+                searchListRequest.Q = "Makakura"; // Replace with your search term.
+                searchListRequest.MaxResults = 50;
 
-            // Call the search.list method to retrieve results matching the specified query term.
-            var searchListResponse = await searchListRequest.ExecuteAsync();
+                // Call the search.list method to retrieve results matching the specified query term.
+                var searchListResponse = await searchListRequest.ExecuteAsync();
 
-            // Add each result to the appropriate list, and then display the lists of
-            // matching videos, channels, and playlists.
-            foreach (var searchResult in searchListResponse.Items)
-            {
-                if (searchResult.Id.Kind == "youtube#video")
-                    lstResult.Add(searchResult);
+                // Add each result to the appropriate list, and then display the lists of
+                // matching videos, channels, and playlists.
+                foreach (var searchResult in searchListResponse.Items)
+                {
+                    if (searchResult.Id.Kind == "youtube#video")
+                        lstResult.Add(searchResult);
+                }
+                lvVideoResult.ItemsSource = lstResult;
             }
-            lvVideoResult.ItemsSource = lstResult;
+            catch (Exception e)
+            {
+
+            }
 
         }
     }
