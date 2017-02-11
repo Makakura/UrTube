3597ce7f5c67d02a530f3733610c6eec41ac5086
Makakura <Makakura@DESKTOP-PKJH420>
2017-02-11 17:30:43 +0700

    34234

---
+++ UrTube/Page2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UrTube
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //Run().Wait();
+        }
+    }
+}
