74263111a7db2ea519e59bed6bda070f0faa475c
Makakura <Makakura@DESKTOP-PKJH420>
2017-02-11 18:12:57 +0700

    4324

--- UrTube/Page2.xaml.cs
+++ UrTube/Page2.xaml.cs
@@ -3,7 +3,8 @@ using System.Windows.Controls;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
 namespace UrTube
 {
     /// <summary>
@@ -11,6 +12,7 @@ namespace UrTube
     /// </summary>
     public partial class Page2 : Page
     {
+        List<Google.Apis.YouTube.v3.Data.SearchResult> lstResult;
         public Page2()
         {
             InitializeComponent();
@@ -18,7 +20,32 @@ namespace UrTube
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            //Run().Wait();
+            Run().Wait();
+        }
+        private async Task Run()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyC5IkscZNJZCEQ_Z8shR80_vi0hFkYS07o",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "Makakura"; // Replace with your search term.
+            searchListRequest.MaxResults = 50;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                if (searchResult.Id.Kind == "youtube#video")
+                    lstResult.Add(searchResult);
+            }
+            lvVideoResult.ItemsSource = lstResult;
+
         }
     }
 }
